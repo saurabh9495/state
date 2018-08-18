16c382f284e3d0d514b069ee344fd7e13a59d20c
Saurabh Kumar <saurabhkumar4829@gmail.com>
2018-08-18 14:22:11 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'state';
+
+  constructor(private http: HttpClient) { }
+
+  public color ="";
+
+  ngOnInit() {
+    let url = `https://api.github.com/users/saurabh9495`;
+    this.http.get(url).subscribe(res => console.log(res));
+  }
 }
